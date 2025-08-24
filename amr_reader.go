29d7f025aed3baac5c4bda957b9e2095871b3f75
prongbang <prongbang@gmail.com>
2025-08-24 11:28:29 +0700

    feat: amr reader

---
+++ amr_reader.go
@@ -0,0 +1,295 @@
+package amrreader
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/innotechdevops/core"
+	"github.com/innotechdevops/timex"
+	"github.com/pkg/errors"
+	"github.com/prongbang/callx"
+)
+
+var hiddenIdList = []string{"__EVENTTARGET", "__EVENTARGUMENT", "__LASTFOCUS", "__VIEWSTATE", "__VIEWSTATEGENERATOR", "__EVENTVALIDATION"}
+
+type Config struct {
+	BaseURL string
+	Host    string
+}
+
+type Credential struct {
+	Username string
+	Password string
+}
+
+func (c *Credential) Checksum() string {
+	sh := sha1.New()
+	sh.Write([]byte(c.Username + ":" + c.Password))
+	return fmt.Sprintf("%x", sh.Sum(nil))
+}
+
+type Account struct {
+	Header       callx.Header
+	MeterNo      string
+	MeterPoint   string
+	CustomerId   string
+	CustomerCode string
+}
+
+type Profile struct {
+	Time              *time.Time `json:"time"`
+	EnergyConsumption *float64   `json:"energyConsumption"`
+}
+
+type ProfileMeter struct {
+	CustomerId   string    `json:"customerId"`
+	CustomerCode string    `json:"customerCode"`
+	MeterNo      string    `json:"meterNo"`
+	MeterPoint   string    `json:"meterPoint"`
+	Profile      []Profile `json:"profile"`
+}
+
+type AmrX interface {
+	Auth(config Credential) (Account, error)
+	GetProfileDaily(acc Account, date string) (ProfileMeter, error)
+}
+
+type amrX struct {
+	Config  Config
+	CallX   callx.CallX
+	session callx.Header
+}
+
+// GetProfileDaily
+// date is supported format "19/09/2024"
+func (a *amrX) GetProfileDaily(acc Account, date string) (ProfileMeter, error) {
+	loc, _ := time.LoadLocation(timex.TimeZoneAsiaBangkok)
+	//fmt.Println(date)
+	query := map[string]string{
+		"Custid":     acc.CustomerId,
+		"CustCode":   acc.CustomerCode,
+		"MeterPoint": acc.MeterPoint,
+		"RepDate":    date,
+		"SumMeter":   "0",
+		"GrphType":   "Col",
+		"DataType":   "0",
+		"kWh":        "1", // กิโลวัตต์ชั่วโมง (kWh)
+		"kVarh":      "0", // กิโลวาร์ชั่วโมง (kVarh)
+		"kW":         "0", // กิโลวัตต์ (kW)
+		"kVar":       "0", // กิโลวาร์ (kVar)
+		"kWh1":       "0",
+		"kVarh1":     "0",
+		"kW1":        "0",
+		"kVar1":      "0",
+		"Cur":        "0",
+		"Vol":        "0",
+		"PF":         "0",
+		"PD":         "0",
+		"chk":        "0",
+	}
+
+	custom := callx.Custom{
+		URL:    a.Config.BaseURL + "/showDailyProfile.aspx?" + core.ToQuery(query),
+		Method: http.MethodGet,
+		Header: acc.Header,
+	}
+	data := a.CallX.Req(custom)
+
+	// Parse html string to dom
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(data.Data)))
+	if err != nil {
+		log.Println("Error loading HTML:", err)
+		return ProfileMeter{}, err
+	}
+
+	divTable := doc.Find("#divTable")
+
+	profileMeter := ProfileMeter{
+		CustomerId:   acc.CustomerId,
+		CustomerCode: acc.CustomerCode,
+		MeterNo:      acc.MeterNo,
+		MeterPoint:   acc.MeterPoint,
+		Profile:      []Profile{},
+	}
+
+	// Find the <tr> and iterate over its <td> elements
+	maxCol := 0
+	divTable.Find("tr").Each(func(row int, s *goquery.Selection) {
+		profile := Profile{}
+
+		s.Find("td").Each(func(col int, td *goquery.Selection) {
+			if row == 0 {
+				maxCol = col
+			} else {
+				text := td.Text()
+				if col == 0 {
+					parsedTime, t := time.ParseInLocation("02/01/2006 15.04", text, loc)
+					if t == nil {
+						profile.Time = &parsedTime
+					}
+				} else if col == maxCol {
+					floatVal, fErr := strconv.ParseFloat(strings.ReplaceAll(text, ",", ""), 64)
+					if fErr == nil {
+						profile.EnergyConsumption = &floatVal
+					}
+				}
+			}
+		})
+
+		if profile.Time != nil {
+			profileMeter.Profile = append(profileMeter.Profile, profile)
+		}
+	})
+
+	slog.Info("GetProfileDaily", "date", date)
+
+	return profileMeter, nil
+}
+
+func (a *amrX) Auth(config Credential) (Account, error) {
+	// Pre-Auth
+	custom := callx.Custom{
+		URL:    a.Config.BaseURL + "/Index.aspx",
+		Method: http.MethodPost,
+		Header: callx.Header{
+			"sec-ch-ua":                 "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+			"sec-ch-ua-mobile":          "?0",
+			"sec-ch-ua-platform":        "\"macOS\"",
+			"Upgrade-Insecure-Requests": "1",
+			"User-Agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+			"Sec-Fetch-Site":            "same-origin",
+			"Sec-Fetch-Mode":            "navigate",
+			"Sec-Fetch-User":            "?1",
+			"Sec-Fetch-Dest":            "document",
+			"host":                      a.Config.Host,
+			"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		},
+	}
+	preAuthRs := a.CallX.Req(custom)
+
+	// Parse html string to dom
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(preAuthRs.Data)))
+	if err != nil {
+		log.Fatal("Error loading HTML:", err)
+	}
+
+	// Post-Auth
+	form := url.Values{
+		"btnOK":       {"เข้าสู่ระบบ"},
+		"txtUsername": {config.Username},
+		"txtPassword": {config.Password},
+		"ddlLanguage": {"th-TH"},
+	}
+
+	// Hidden input
+	for _, id := range hiddenIdList {
+		value, exists := doc.Find(fmt.Sprintf("#%s", id)).Attr("value")
+		if exists {
+			form.Set(id, value)
+		}
+	}
+	custom.Header["Content-Type"] = "application/x-www-form-urlencoded"
+
+	// Get cookies session id
+	re := regexp.MustCompile(`ASP\.NET_SessionId=([^;]+)`)
+	match := re.FindStringSubmatch(preAuthRs.Cookies["ASP.NET_SessionId"])
+	if len(match) > 1 {
+		custom.Header["Cookie"] = fmt.Sprintf("ASP.NET_SessionId=%s", match[1])
+	}
+	custom.Form = strings.NewReader(form.Encode())
+
+	slog.Info("Request:")
+	slog.Info(fmt.Sprintf("	POST -> %s", custom.URL))
+	slog.Info(fmt.Sprintf("	Header: %s", custom.Header))
+	slog.Info(fmt.Sprintf("	Form: %s", form.Encode()))
+
+	postAuthRs := a.CallX.Req(custom)
+
+	slog.Info("Response:")
+	slog.Info(fmt.Sprintf("	Status %d %s", postAuthRs.Code, http.StatusText(postAuthRs.Code)))
+	slog.Info(fmt.Sprintf("	Cookie %s", custom.Header["Cookie"]))
+
+	if postAuthRs.Code == http.StatusFound {
+		mainCustCustom := callx.Custom{URL: a.Config.BaseURL + "/MainCust.aspx", Method: http.MethodGet, Header: custom.Header}
+
+		slog.Info("Request:")
+		slog.Info(fmt.Sprintf("	GET -> %s", mainCustCustom.URL))
+		slog.Info(fmt.Sprintf("	Header: %s", mainCustCustom.Header))
+
+		mainCustRs := a.CallX.Req(mainCustCustom)
+
+		slog.Info("Response:")
+		slog.Info(fmt.Sprintf("	Status %d %s", mainCustRs.Code, http.StatusText(mainCustRs.Code)))
+
+		// Parse html string to dom
+		docCust, errCust := goquery.NewDocumentFromReader(strings.NewReader(string(mainCustRs.Data)))
+		if errCust != nil {
+			log.Fatal("Error loading HTML:", errCust)
+		}
+
+		custUrl, exists := docCust.Find("#frmMain").Attr("src")
+		if exists {
+			custUrl = strings.ReplaceAll(custUrl, "./", a.Config.BaseURL+"/")
+			queryParams := core.Query(custUrl)
+			custID := queryParams.Get("Custid")
+			meterNo := queryParams.Get("PeaNo")
+			custDashboardCustom := callx.Custom{URL: custUrl, Method: http.MethodGet, Header: custom.Header}
+
+			slog.Info("Request:")
+			slog.Info(fmt.Sprintf("	GET -> %s", custDashboardCustom.URL))
+			slog.Info(fmt.Sprintf("	Header: %s", custDashboardCustom.Header))
+
+			// Get meter no and meter point
+			custDashboardRs := a.CallX.Req(custDashboardCustom)
+
+			slog.Info("Response:")
+			slog.Info(fmt.Sprintf("	Status %d %s", mainCustRs.Code, http.StatusText(mainCustRs.Code)))
+
+			// Parse html string to dom
+			docDash, errDash := goquery.NewDocumentFromReader(strings.NewReader(string(custDashboardRs.Data)))
+			if errDash != nil {
+				log.Fatal("Error loading HTML:", errCust)
+			}
+
+			meterPoint := docDash.Find("select#ddlMeter option[selected]").AttrOr("value", "")
+			meterNo = docDash.Find("select#ddlMeter option[selected]").Text()
+
+			// reset session
+			a.session = custom.Header
+
+			return Account{
+				Header:       custom.Header,
+				CustomerId:   custID,
+				CustomerCode: config.Username,
+				MeterNo:      meterNo,
+				MeterPoint:   meterPoint,
+			}, nil
+		} else {
+			slog.Error("Id frmMain not found")
+		}
+	} else {
+		slog.Error("Auth error")
+	}
+
+	// reset session
+	a.session = callx.Header{}
+
+	return Account{}, errors.New(string(preAuthRs.Data))
+}
+
+func New(config Config, callX callx.CallX) AmrX {
+	return &amrX{
+		Config:  config,
+		CallX:   callX,
+		session: callx.Header{},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/innotechdevops/amr-reader
+
+go 1.23.4
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3
+	github.com/innotechdevops/core v1.0.5
+	github.com/innotechdevops/timex v1.0.1
+	github.com/pkg/errors v0.9.1
+	github.com/prongbang/callx v1.3.3
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.52.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+)
