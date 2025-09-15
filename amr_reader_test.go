0087f10c19e9bd23eefd3e0043f0aac688606361
prongbang <prongbang@gmail.com>
2025-09-15 21:55:06 +0700

    feat(password-rotation): implement logic change password

--- amr_reader_test.go
+++ amr_reader_test.go
@@ -42,7 +42,7 @@ func TestAuth(t *testing.T) {
 	}
 }
 
-func TestAMR(t *testing.T) {
+func TestGetAccount(t *testing.T) {
 	callX := callx.New(callx.Config{
 		Timeout:            60,
 		Cookies:            true,
@@ -72,4 +72,44 @@ func TestAMR(t *testing.T) {
 	acc, err := amr.GetAccount(session)
 	fmt.Println("error:", err)
 	fmt.Println("account:", acc)
+}
+
+func TestPasswordRotation(t *testing.T) {
+	callX := callx.New(callx.Config{
+		Timeout:            60,
+		Cookies:            true,
+		InsecureSkipVerify: true,
+	})
+
+	config := amrreader.Config{
+		BaseURL: os.Getenv("AMR_BASE_URL"),
+		Logger:  true,
+	}
+	amr := amrreader.New(config, callX)
+
+	// Read last header
+	b, err := os.ReadFile(".header")
+	if err != nil {
+		t.Error("can't read the .header")
+	}
+	session := amrreader.Session{
+		Username: os.Getenv("AMR_USERNAME"),
+	}
+	err = json.Unmarshal(b, &session.Header)
+	if err != nil {
+		t.Error("can't parse the .header to map")
+	}
+
+	// Change password
+	credential := amrreader.PasswordRotationCredential{
+		Username:    session.Username,
+		OldPassword: os.Getenv("AMR_PASSWORD"),
+		NewPassword: os.Getenv("AMR_NEW_PASSWORD"),
+	}
+	newSession, err := amr.PasswordRotation(credential, &session.Header)
+	fmt.Println("err:", err)
+	fmt.Println("newSession:", newSession)
+	if err == nil {
+		fmt.Println("password changed:", credential.NewPassword)
+	}
 }
